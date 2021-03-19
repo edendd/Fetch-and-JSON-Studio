1eb3f14b6d48d5f99531fcba490341106646eac9
edenhailu333@gmail.com <EDENhailu21>
2021-03-18 23:15:16 -0500

    JSON

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 // TODO: add code here
+
+window.addEventListener("load", function(){
+  let json= [];
+fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+response.json().then( function(json) {
+
+       const astro = document.getElementById("container");
+      astrocounter = 0;
+       let asto2 = ''
+     for (const astronunt of json){
+        
+
+
+        astrocounter++
+        asto2 +=`
+
+        <div class="astronaut">
+        <div class="bio">
+        <h3> ${astrocounter}. ${astronunt.firstName} ${astronunt.lastName}</h3>
+        <ul>
+         <li>Hours in space: ${astronunt.hoursInSpace}</li>
+         <li>Active: ${astronunt.active}</li>
+         <li>Skills: ${astronunt.skills}</li>
+     </ul>
+   
+      </div>
+      <img class="avatar" src=${astronunt.picture}>
+      </div>
+        
+        `;
+     
+     }
+
+     astro.innerHTML=asto2;
+
+    })
+
+  })
+
+})
